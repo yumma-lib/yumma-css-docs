5d4f36d4988d8ab30c197d5a64ef9171c8e02c2b
Renildo Pereira <rrenildopereiraa@gmail.com>
2023-09-20 23:46:46 +0100

    Yumma CSS Docs v0.0.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
-          editUrl: "https://github.com/rrenildopereiraa/yumma-css-docs",
+          editUrl: "https://github.com/yumma-lib/yumma-css-docs",
           // includeCurrentVersion: false
         },
         blog: false,
