3708f40857644ad5d66d84db94d055ad5e612faf
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-01-22 00:36:50 +0000

    Updated GridSection component

---
+++ src/components/CardSection/CardSection.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './CardSection.module.css';
+
+const CardSection = ({ cardData }) => {
+    return (
+        <div className={styles.cnn}>
+            {cardData.map((card, index) => (
+                <div className={styles.card} key={index}>
+                    <img src={card.cardIcon} alt={card.cardTitle} />
+                    <h2>{card.cardTitle}</h2>
+                    <p>{card.cardDesc}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+CardSection.propTypes = {
+    cardData: PropTypes.arrayOf(
+        PropTypes.shape({
+            cardIcon: PropTypes.string.isRequired,
+            cardTitle: PropTypes.string.isRequired,
+            cardDesc: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
+
+export default CardSection;
