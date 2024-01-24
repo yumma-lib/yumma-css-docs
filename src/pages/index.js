7417489a15e0fac57431ea1ca4698670aace76fb
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-01-24 22:18:53 +0000

    Index page improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './index.module.css';
+import styles from './style.module.css';
 import Link from "@docusaurus/Link";
 import CardSection from '@site/src/components/CardSection/CardSection';
 
