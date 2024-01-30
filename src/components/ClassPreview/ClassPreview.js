012bca7591d9bd99870d188ee24bcb46a97ef657
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-01-30 14:43:21 +0000

    Added input component page

--- src/components/ClassPreview/ClassPreview.js
+++ src/components/ClassPreview/ClassPreview.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ClassPreview = ({ codeData, isPadded = false, isCentered = false, isVersion = '' }) => {
+const ClassPreview = ({ codeData, isPadded = false, isCentered = false, isVersion = '', isHeight }) => {
+  
   const noRightClick = (event) => {
     const { target } = event;
     if (target.tagName === 'A' && target.getAttribute('href') === '#') {
@@ -17,24 +18,23 @@ const ClassPreview = ({ codeData, isPadded = false, isCentered = false, isVersio
     align-items: ${isCentered ? 'center' : 'flex-start'};
     ${isPadded ? 'padding: 12px 14px;' : ''}
   `;
-  const stylesheet = `https://cdn.jsdelivr.net/npm/yummacss@${isVersion}/dist/yumma.css`;
+  const stylesheet = `https://cdn.jsdelivr.net/gh/rrenildopereiraa/yumma-css@${isVersion}/dist/yumma.css`;
 
   const iframe = `
     <html>
       <head>
         <link rel="stylesheet" href="${stylesheet}">
-        <style> body { margin: 0; } .container { ${styles} } </style>
         <script> window.addEventListener('click', ${noRightClick}); </script>
       </head>
       <body>
-        <div class="container">${codeData}</div>
+        <div style="${styles}">${codeData}</div>
       </body>
     </html>
   `;
 
   const iframeStyle = {
     width: '100%',
-    height: '200px',
+    height: isHeight || '200px',
     border: '1px solid #ccc',
     backgroundColor: '#f9f9f9',
     boxShadow: 'rgba(0, 0, 0, 0.0) 0px 2px 4px, rgba(0, 0, 0, 0.1) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
@@ -56,6 +56,7 @@ ClassPreview.propTypes = {
   isPadded: PropTypes.bool,
   isCentered: PropTypes.bool,
   isVersion: PropTypes.string,
+  isHeight: PropTypes.string,
 };
 
 export default ClassPreview;
