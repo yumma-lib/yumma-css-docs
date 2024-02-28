7c17848bc6660bd4f6245541923c9ddd3f299632
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-02-28 17:58:31 +0000

    Added pwa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const vortyx = require('./src/themes/vortyx');
 const config = {
   title: 'Yumma CSS',
   tagline: 'The documentation site for Yumma CSS.',
-  favicon: '/img/yummacss.ico',
+  favicon: '/img/meta/favicon.ico',
   url: 'https://yummacss.com',
   baseUrl: '/',
   organizationName: 'yumma-lib',
