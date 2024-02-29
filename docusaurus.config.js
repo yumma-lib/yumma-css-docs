255e2b2d757bfa26284fe380eea2c2a2863aba6f
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-02-29 10:58:06 +0000

    Small pwa changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -150,22 +150,64 @@ const config = {
       },
       plugins: [
         [
-          '@docusaurus/plugin-pwa',
+          'pwa',
           {
+            offlineModeActivationStrategies: [
+              'appInstalled',
+              'standalone',
+              'queryString',
+            ],
+            swCustom: require.resolve('./src/sw.js'),
             pwaHead: [
               {
                 tagName: 'link',
-                rel: 'apple-touch-icon',
-                href: 'img/meta/apple-touch-icon.png'
+                rel: 'icon',
+                href: 'img/yummacss.png',
               },
               {
                 tagName: 'link',
                 rel: 'manifest',
-                href: 'manifest.json'
-              }
+                href: 'manifest.json',
+              },
+              {
+                tagName: 'meta',
+                name: 'theme-color',
+                content: 'rgb(221, 9, 135)',
+              },
+              {
+                tagName: 'meta',
+                name: 'apple-mobile-web-app-capable',
+                content: 'yes',
+              },
+              {
+                tagName: 'meta',
+                name: 'apple-mobile-web-app-status-bar-style',
+                content: '#000',
+              },
+              {
+                tagName: 'link',
+                rel: 'apple-touch-icon',
+                href: 'img/yummacss.png',
+              },
+              {
+                tagName: 'link',
+                rel: 'mask-icon',
+                href: 'img/yummacss.png',
+                color: 'rgb(221, 9, 135)',
+              },
+              {
+                tagName: 'meta',
+                name: 'msapplication-TileImage',
+                content: 'img/yummacss.png',
+              },
+              {
+                tagName: 'meta',
+                name: 'msapplication-TileColor',
+                content: '#fff',
+              },
             ],
           },
-        ],
+        ]
       ],
     }),
 };
