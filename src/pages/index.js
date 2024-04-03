ad782974f52fca90c29519cb18007acefb5b513b
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-04-03 14:02:29 +0100

    Homepage improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,79 +1,79 @@
-import styles from './index.modules.css';
-import logo from '@site/static/img/yummacss.png';
-
-import GridList from '@site/src/components/GridList/GridList';
-
-import React from "react";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import GridList from '@site/src/components/GridList/GridList';
+import logo from '@site/static/img/yummacss.png';
+import Layout from "@theme/Layout";
+import React from "react";
 
-const Index = () => {
+import styles from './index.modules.css';
+
+export default function Index() {
     const { siteConfig } = useDocusaurusContext();
 
     return (
-        <div>
-            <div className={styles.yma__header}>
-                <div>
-                    <img src={logo} alt="Yumma CSS Logo" />
-                    <h1 className={styles.yma__heading}>{siteConfig.title}</h1>
-                    <p className={styles.yma__description}>Build beautiful websites with small class names</p>
-                    <Link className={styles.yma__primary__button} to='/docs/intro'>Read Docs</Link>
-                    <Link className={styles.yma__secondary__button} to='/playground'>Try It Now</Link>
+        <Layout>
+            <div>
+                <div className={styles.yma__header}>
+                    <div>
+                        <img src={logo} alt={`${siteConfig.title} Logo`} />
+                        <h1 className={styles.yma__heading}>{siteConfig.title}</h1>
+                        <p className={styles.yma__description}>Build beautiful websites with small class names</p>
+                        <Link className={styles.yma__primary__button} to='/docs/intro'>Read Docs</Link>
+                        <Link className={styles.yma__secondary__button} to='/playground'>Try It Now</Link>
+                    </div>
                 </div>
+
+                <section className={styles.yma__section}>
+                    <h1 className={styles.yma__heading}>Awesome Features</h1>
+                    <p className={styles.yma__description}>Discover why Yumma CSS is so special</p>
+
+                    <GridList
+                        cardData={[
+                            {
+                                icon: `${require('@site/static/img/Homepage/components.png').default}`,
+                                title: 'Components',
+                                description: 'Explore our components to add buttons, navbars, cards, and other elements to your interface.'
+                            },
+                            {
+                                icon: `${require('@site/static/img/Homepage/responsiveness.png').default}`,
+                                title: 'Responsiveness',
+                                description: 'Customize your design elements with the help of a responsive design system.'
+                            },
+                            {
+                                icon: `${require('@site/static/img/Homepage/productivity.png').default}`,
+                                title: 'Productivity',
+                                description: 'Get Yumma CSS Intelephense in your Visual Studio Code and boost your development.'
+                            },
+                            {
+                                icon: `${require('@site/static/img/Homepage/themes.png').default}`,
+                                title: 'Themes',
+                                description: 'Use the color palette for amazing applications and create your own themes.'
+                            },
+                            {
+                                icon: `${require('@site/static/img/Homepage/utilities.png').default}`,
+                                title: 'Utilities',
+                                description: 'Advanced tiny utility classes that you can use on your applications.'
+                            },
+                            {
+                                icon: `${require('@site/static/img/Homepage/playground.png').default}`,
+                                title: 'Playground',
+                                description: 'Our official real-time code editor with Yumma CSS integrated into it.'
+                            },
+                        ]}
+                    />
+                </section>
+
+                <section className={styles.yma__header}>
+                    <h1 className={styles.yma__heading}>Interface Components</h1>
+                    <p className={styles.yma__description}>Use or build your own interface components using classes like <code>d-f</code>, <code>nav-white</code>, <code>t-lead</code> and much more!</p>
+                </section>
+
+                <section className={styles.yma__section}>
+                    <h1 className={styles.yma__heading}>Getting Started</h1>
+                    <p className={styles.yma__description}>Start by including Yumma CSS in your project</p>
+                    <pre>npm i yummacss@latest</pre>
+                </section>
             </div>
-
-            <section className={styles.yma__section}>
-                <h1 className={styles.yma__heading}>Awesome Features</h1>
-                <p className={styles.yma__description}>Discover why Yumma CSS is so special</p>
-                <GridList
-                    cardData={[
-                        {
-                            icon: `${require('@site/static/img/Homepage/components.png').default}`,
-                            title: 'Components',
-                            description: 'Explore our components to add buttons, navbars, cards, and other elements to your interface.'
-                        },
-                        {
-                            icon: `${require('@site/static/img/Homepage/responsiveness.png').default}`,
-                            title: 'Responsiveness',
-                            description: 'Customize your design elements with the help of a responsive design system.'
-                        },
-                        {
-                            icon: `${require('@site/static/img/Homepage/productivity.png').default}`,
-                            title: 'Productivity',
-                            description: 'Get Yumma CSS Intelephense in your Visual Studio Code and boost your development.'
-                        },
-                        {
-                            icon: `${require('@site/static/img/Homepage/themes.png').default}`,
-                            title: 'Themes',
-                            description: 'Use the color palette for amazing applications and create your own themes.'
-                        },
-                        {
-                            icon: `${require('@site/static/img/Homepage/utilities.png').default}`,
-                            title: 'Utilities',
-                            description: 'Advanced tiny utility classes that you can use on your applications.'
-                        },
-                        {
-                            icon: `${require('@site/static/img/Homepage/playground.png').default}`,
-                            title: 'Playground',
-                            description: 'Our official real-time code editor with Yumma CSS integrated into it.'
-                        },
-                    ]}
-                />
-            </section>
-
-            <section className={styles.yma__header}>
-                <h1 className={styles.yma__heading}>Interface Components</h1>
-                <p className={styles.yma__description}>Use or build your own interface components using classes like <code>d-f</code>, <code>nav-white</code>, <code>t-lead</code> and much more!</p>
-
-            </section>
-
-            <section className={styles.yma__section}>
-                <h1 className={styles.yma__heading}>Getting Started</h1>
-                <p className={styles.yma__description}>Start by including Yumma CSS in your project</p>
-                <pre>npm i yummacss@latest</pre>
-            </section>
-        </div >
+        </Layout>
     );
-};
-
-export default Index;
+}
