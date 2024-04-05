afa90b70d82c48602d9f90a24a36e05c927d7168
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-04-05 22:49:15 +0100

    Removed Edit page link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,6 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: "https://github.com/yumma-lib/yumma-css-docs/blob/release",
           includeCurrentVersion: false
         },
         blog: {
