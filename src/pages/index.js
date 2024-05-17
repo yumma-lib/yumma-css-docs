d93d94e692673af7d20380ea978c853714b96351
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-05-17 12:45:52 +0100

    remove: delete index.module.css file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,20 @@
 import Link from "@docusaurus/Link";
 import Layout from "@theme/Layout";
 import React from "react";
-import { IoPlayOutline } from "react-icons/io5";
 
-import styles from './index.module.css';
+import "yummacss/dist/yumma.css";
 
 export default function Index() {
 
     return (
         <Layout>
-            <div style={{ margin: '12px' }}>
-                <div className={styles.yma__header}>
-                    <div>
-                        <h1 className={styles.yma__heading}>Quickly build amazing websites with less code in your markup.</h1>
-                        <p className={styles.yma__paragraph}>Bundled with components and small classes that are designed for fast development with small class names such as ins, btn, d-f, and many more.</p>
-                        <div className={styles.yma__action}>
-                            <Link className={styles.yma__btn} to='/docs/installation'>Get started</Link>
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.yma__video}>
-                    <img className={styles.yma__video__thumbnail} src="/img/thumbnail.png" alt="Yumma CSS Video Thumbnail" />
-                    <div className={styles.yma__video__container}>
-                        <a className={styles.yma__video__btn} href="#">
-                            <IoPlayOutline className={styles.yma__video__btn__icon} />
-                            Play the overview
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <main style={{ maxWidth: '56rem' }} className="d-f fd-c jc-c lg:mb-10 lg:mt-8 mb-6 mt-4 mr-auto ml-auto ta-c">
+                <h1 className="as-c d-f fs-6xl fw-600 lg:text-6xl lh-1">Quickly build amazing websites with less code in your markup.</h1>
+                <p className="as-c fs-2xl t-lead lh-3 max-w-100 md:max-w-full ta-c">Bundled with components and small class names that are designed for fast development.</p>
+                <section className="as-c d-f fd-c mt-2 sm:fd-r sm:w-auto w-full">
+                    <Link className="ai-c bg-pink h:t-white d-if fs-md fw-700 jc-c lh-3 px-1 py-1 rad-8 sm:px-2 sm:w-auto t-white w-full" to="/docs/installation">Get started</Link>
+                </section>
+            </main>
         </Layout>
     );
 }
