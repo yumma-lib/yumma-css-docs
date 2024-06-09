17780b8b7b8d1dc13efa46da96138785df03fa79
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-06-09 16:23:26 +0100

    refactor: using ClassPreview component in Components pages

--- src/components/ClassPreview/ClassPreview.js
+++ src/components/ClassPreview/ClassPreview.js
@@ -1,58 +1,47 @@
 import { useColorMode } from '@docusaurus/theme-common';
 import CodeBlock from '@theme/CodeBlock';
 import PropTypes from 'prop-types';
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
+import beautify from 'js-beautify';
 
-const ClassPreview = ({ codeData, isPadded = false, isCentered = false, isVersion = '', isHeight, hideScroll = false }) => {
+const ClassPreview = ({ codeData, isPadded = false, isCentered = false, setVersion = '', isHeight, hideScroll = false }) => {
   const iframeRef = useRef(null);
-
-  useEffect(() => {
-    const noRightClick = (event) => {
-      event.preventDefault();
-    };
-
-    const iframeDocument = iframeRef.current.contentDocument || iframeRef.current.contentWindow.document;
-    iframeDocument.addEventListener('contextmenu', noRightClick);
-
-    return () => {
-      iframeDocument.removeEventListener('contextmenu', noRightClick);
-    };
-  }, []);
 
   const { colorMode } = useColorMode();
   const flex = isCentered ? 'display: flex;' : '';
-  const stylesheet = `https://cdn.jsdelivr.net/gh/yumma-lib/yumma-css@${isVersion}/dist/yumma.css`;
+  const stylesheet = `https://cdn.jsdelivr.net/gh/yumma-lib/yumma-css@${setVersion}/dist/yumma.css`;
 
   const styles = `
-    ${flex}
-    justify-content: ${isCentered ? 'center' : 'flex-start'};
-    align-items: ${isCentered ? 'center' : 'flex-start'};
+    ${flex} justify-content: ${isCentered ? 'center' : 'flex-start'}; align-items: ${isCentered ? 'center' : 'flex-start'};
     ${isPadded ? 'padding: 12px 14px;' : ''}
     ${hideScroll ? 'overflow-y: hidden;' : ''}
   `;
 
   const formatHTML = (html) => {
-    return html.replace(/>\s+</g, '>\n<').trim();
+    return beautify.html(html, {
+      indent_size: 4,
+      wrap_line_length: 120,
+      preserve_newlines: false,
+    });
   };
 
   const processedCode = formatHTML(codeData);
 
-  const iframe = `
-    <html>
-      <head>
-        <link rel="stylesheet" href="${stylesheet}">
-        <style>
-          ${hideScroll ? 'body { overflow-y: hidden; }' : ''}
-          a[href="#"] { pointer-events: none; cursor: default; } /* Prevent navigation for anchor tags with href="#" */
-        </style>
-      </head>
-      <body>
-        <div style="${styles}">${processedCode}</div>
-      </body>
-    </html>
-  `;
+  const container = `
+  <html>
+    <head>
+      <link rel="stylesheet" href="${stylesheet}">
+      <style>
+        ${hideScroll ? 'body { overflow-y: hidden; }' : ''}
+      </style>
+    </head>
+    <body>
+      <div style="${styles}">${processedCode}</div>
+    </body>
+  </html>
+`;
 
-  const iframeStyle = {
+  const styling = {
     backgroundColor: colorMode === 'dark' ? '#1d2026' : '#f9fafb',
     border: colorMode === 'dark' ? '1px solid #2d2f33' : '1px solid #e5e7eb',
     borderRadius: '8px',
@@ -66,8 +55,8 @@ const ClassPreview = ({ codeData, isPadded = false, isCentered = false, isVersio
       <iframe
         ref={iframeRef}
         sandbox="allow-scripts allow-same-origin"
-        style={iframeStyle}
-        srcDoc={iframe}
+        style={styling}
+        srcDoc={container}
         loading="lazy"
         rel="noopener noreferrer"
       />
@@ -82,7 +71,7 @@ ClassPreview.propTypes = {
   codeData: PropTypes.string.isRequired,
   isPadded: PropTypes.bool,
   isCentered: PropTypes.bool,
-  isVersion: PropTypes.string,
+  setVersion: PropTypes.string,
   isHeight: PropTypes.string,
   hideScroll: PropTypes.bool,
 };
