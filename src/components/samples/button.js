4042bf2443b69b05f97ee9ca354c0216ffe0b2a0
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-07-03 01:17:24 +0100

    docs: add button components

---
+++ src/components/samples/button.js
@@ -0,0 +1,23 @@
+export const button = `
+<button class="ai-c bg-blue cg-2 c-p d-if fs-sm fw-600 c-p px-4 py-3 rad-2 t-white">
+  Button
+</button>
+`;
+
+export const outlineButton = `
+<button class="ai-c b-1 bc-blue bg-transparent cg-2 c-p d-if fs-sm fw-600 h:bc-d-blue-3 h:t-d-blue-3 px-4 py-3 rad-2 t-blue">
+  Button
+</button>
+`;
+
+export const ghostButton = `
+<button class="ai-c bg-transparent cg-2 c-p d-if fs-sm fw-600 h:bg-l-blue-6 px-4 py-3 rad-2 t-blue">
+  Button
+</button>
+`;
+
+export const blockButton = `
+<button class="ai-c bg-blue cg-2 c-p d-if fs-sm fw-600 h:bg-d-blue-2 jc-c px-4 py-3 rad-2 t-white w-full">
+  Button
+</button>
+`;
