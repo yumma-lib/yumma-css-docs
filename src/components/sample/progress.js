bbd33890f350e1220dec8e46aef1b479f17a7b68
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-07-05 13:03:27 +0100

    docs: update progress components page

--- src/components/sample/progress.js
+++ src/components/sample/progress.js
@@ -1,34 +1,29 @@
 export const basic = `
-<div class="bg-l-gray-6 d-f h-4 ovf-h rad-8 w-full">
+<div class="bg-l-gray-6 d-f h-3 ovf-h rad-8 w-full">
   <div class="bg-blue d-f fd-c fs-xs jc-c ovf-h rad-8 t-white ta-c w-half"></div>
 </div>
 `;
 
 export const withLabels = `
-<div class="bg-l-gray-6 d-f h-4 mb-3 ovf-h rad-8 w-full">
-  <div class="bg-blue d-f fd-c fs-xs jc-c ovf-h rad-8 py-1 t-white ta-c w-half">50%</div>
-</div>
+<div class="s-y-3">
+  <div>
+    <div class="ai-c d-f jc-sb mb-2">
+      <h3 class="fs-sm fw-600 t-gray">Uploading file...</h3>
+      <span class="fs-sm t-gray">50%</span>
+    </div>
+    <div class="bg-l-gray-6 d-f h-3 ovf-h rad-8 w-full">
+      <div class="bg-blue d-f fd-c fs-xs jc-c ovf-h rad-8 t-white ta-c w-half"></div>
+    </div>
+  </div>
 
-<div class="bg-l-gray-6 d-f h-4 ovf-h rad-8 w-full">
-  <div class="bg-blue d-f fd-c fs-xs jc-c ovf-h rad-8 py-1 t-white ta-c w-full">100%</div>
-</div>
-`;
-
-export const withEndLabels = `
-<div class="ai-c cg-3 d-f">
-  <div class="bg-l-gray-6 d-f h-4 ovf-h rad-8 w-full">
-    <div class="bg-blue d-f fd-c fs-xs jc-c ovf-h rad-8 t-white ta-c w-half"></div>
-  </div>
-  <div class="ta-e w-10">
-    <span class="fs-sm fw-600 t-gray">50%</span>
-  </div>
-</div>
-<div class="ai-c cg-3 d-f">
-  <div class="bg-l-gray-6 d-f h-4 ovf-h rad-8 w-full">
-    <div class="bg-blue d-f fd-c fs-xs jc-c ovf-h rad-8 t-white ta-c w-full"></div>
-  </div>
-  <div class="ta-e w-10">
-    <span class="fs-sm fw-600 t-gray">100%</span>
+  <div>
+    <div class="ai-c d-f jc-sb mb-2">
+      <h3 class="fs-sm fw-600 t-gray">Your upload is complete</h3>
+      <span class="fs-sm t-gray">100%</span>
+    </div>
+    <div class="bg-l-gray-6 d-f h-3 ovf-h rad-8 w-full">
+      <div class="bg-blue d-f fd-c fs-xs jc-c ovf-h rad-8 t-white ta-c w-full"></div>
+    </div>
   </div>
 </div>
 `;
