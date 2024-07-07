993ad240f0bb29ade99cf2129f6336c12427d874
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-07-07 22:46:32 +0100

    docs: add tab components

--- src/components/sample/banner.js
+++ src/components/sample/banner.js
@@ -14,10 +14,10 @@ export const rightAligned = `
   <div class="bg-white bs-sm p-4 rad-2">
     <div class="cg-5 d-f">
       <div class="fg-1">
-        <h2 class="fs-lg fw-600 t-d-gray-4">
+        <h2 class="fs-lg fw-600 t-d-silver-4">
           We are using cookies to improve your experience!
         </h2>
-        <p class="fs-sm mt-2 t-d-gray-1">By clicking "Allow all", you agree to use of all cookies. Visit our
+        <p class="fs-sm mt-2 t-d-silver-1">By clicking "Allow all", you agree to use of all cookies. Visit our
           <a class="ai-c d-if fw-500 gc-3 t-blue tdl-u" href="#">Cookies Policy</a>
           to learn more.
         </p>
@@ -25,7 +25,7 @@ export const rightAligned = `
           <button type="button" class="ai-c bg-blue c-p cg-2 d-if fs-sm fw-600 h:bg-d-blue-2 px-3 py-2 rad-2 t-white">
             Allow all
           </button>
-          <button type="button" class="ai-c b-1 bc-l-gray-6 bg-white bs-sm c-p cg-2 d-if fs-sm fw-500 px-3 py-2 rad-2 t-d-gray-4">
+          <button type="button" class="ai-c b-1 bc-l-silver-6 bg-white bs-sm c-p cg-2 d-if fs-sm fw-500 px-3 py-2 rad-2 t-d-silver-4">
             Manage cookies
           </button>
         </div>
@@ -39,17 +39,17 @@ export const fullWidthCentralized = `
 <div class="dir-b-0 dir-l-0 dir-r-0 p-f z-60">
   <div class="bg-white bs-sm p-4">
     <div class="max-w-96 mx-auto w-full">
-      <h2 class="fw-600 t-d-gray-4">
+      <h2 class="fw-600 t-d-silver-4">
         Cookie Settings
       </h2>
-      <p class="fs-sm mt-2 t-d-gray-4">
+      <p class="fs-sm mt-2 t-d-silver-4">
         The use of cookies serves to optimize the user experience and facilitate targeted marketing initiatives.
         For further information, please refer to our
         <a class="ai-c cg-3 d-if fw-500 t-blue tdl-u" href="#">Cookies Policy</a>
       </p>
       <div class="cg-2 d-if mt-5">
         <button type="button" class="ai-c bg-blue c-p cg-2 d-if fs-sm fw-600 h:bg-d-blue-2 px-3 py-2 rad-2 t-white">Allow all</button>
-        <button type="button" class="ai-c b-1 bc-l-gray-5 bg-white bs-sm c-p cg-2 d-if fs-sm fw-500 px-3 py-2 rad-2 t-d-gray-6">Manage cookies</button>
+        <button type="button" class="ai-c b-1 bc-l-silver-5 bg-white bs-sm c-p cg-2 d-if fs-sm fw-500 px-3 py-2 rad-2 t-d-silver-6">Manage cookies</button>
       </div>
     </div>
   </div>
--- src/components/sample/card.js
+++ src/components/sample/card.js
@@ -1,10 +1,10 @@
 export const basic = `
-<div class="b-1 bc-l-gray-6 bg-white bs-sm d-ib fd-c rad-2 max-w-70">
+<div class="b-1 bc-l-silver-6 bg-white bs-sm d-ib fd-c rad-2 max-w-70">
   <div class="md:p-7 p-4">
-    <h3 class="fs-lg fw-700 t-d-gray-4">
+    <h3 class="fs-lg fw-700 t-d-silver-4">
       Gilbert Jobs
     </h3>
-    <p class="mt-2 t-gray ">
+    <p class="mt-2 t-silver ">
       I am a Fullstack Web Developer who is now working on some personal projects.
     </p>
     <a class="ai-c cg-1 d-if fs-sm fw-600 h:t-d-blue-2 mt-3 rad-2 t-blue" href="#">
@@ -18,13 +18,13 @@ export const basic = `
 `;
 
 export const withImage = `
-<div class="d-f fd-c bg-white b-1 bs-sm rad-2 bc-l-gray-6 max-w-70">
+<div class="d-f fd-c bg-white b-1 bs-sm rad-2 bc-l-silver-6 max-w-70">
   <img class="w-full h-auto rad-tl-1 rad-tr-1" src="https://images.pexels.com/photos/305821/pexels-photo-305821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1">
   <div class="p-4 md:p-5">
-    <h3 class="fs-lg fw-700 t-d-gray-4">
+    <h3 class="fs-lg fw-700 t-d-silver-4">
       Save the Planet
     </h3>
-    <p class="mt-1 t-gray">
+    <p class="mt-1 t-silver">
       By combining our strengths, we can achieve global impact and create a more positive future for future generations.
     </p>
     <a class="ai-c cg-1 d-if fs-sm fw-600 h:t-d-blue-2 mt-3 rad-2 t-blue" href="#">
@@ -38,12 +38,12 @@ export const withImage = `
 `;
 
 export const centralized = `
-<div class="b-1 bc-l-gray-6 bg-white bs-sm d-f fd-c min-h-60 rad-2">
+<div class="b-1 bc-l-silver-6 bg-white bs-sm d-f fd-c min-h-60 rad-2">
   <div class="f-1 f-auto fd-c ins md:p-5 p-4">
-    <h3 class="fs-lg fw-700 t-d-gray-4">
+    <h3 class="fs-lg fw-700 t-d-silver-4">
       User Management
     </h3>
-    <p class="mt-2 t-gray">
+    <p class="mt-2 t-silver">
       A quick and easy way to manage users, including password resets, blocking, and deleting users.
     </p>
     <a class="c-p ai-c b-1 bc-transparent cg-1 d-if fs-sm fw-600 h:t-d-blue-2 href= mt-3 rad-2 t-blue" href="#">
@@ -57,7 +57,7 @@ export const centralized = `
 `;
 
 export const centralizedWithIcon = `
-<div class="b-1 bc-l-gray-6 bg-white bs-sm d-f fd-c min-h-60 rad-2">
+<div class="b-1 bc-l-silver-6 bg-white bs-sm d-f fd-c min-h-60 rad-2">
   <div class="f-1 f-auto fd-c ins md:p-5 p-4">
     <svg class="dim-10 t-gray" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M5 12V18C5 18 5 21 12 21C19 21 19 18 19 18V12" stroke="currentColor" stroke-width="1.5"></path><path d="M5 6V12C5 12 5 15 12 15C19 15 19 12 19 12V6" stroke="currentColor" stroke-width="1.5"></path><path d="M12 3C19 3 19 6 19 6C19 6 19 9 12 9C5 9 5 6 5 6C5 6 5 3 12 3Z" stroke="currentColor" stroke-width="1.5"></path>
