84880ae830f7c086012c5ac3ab95e3cab261d389
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-07-07 17:14:32 +0100

    docs: update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,10 @@ export default function Index() {
         <Layout description={description}>
             <div className="mx-2">
                 <main style={{ maxWidth: '56rem' }} className="d-f fd-c jc-c lg:mb-30 lg:mt-24 mb-18 mt-12 mx-auto ta-c">
-                    <h1 className="as-c d-f fs-6xl fw-600 lg:text-6xl lh-1">Quickly build applications with less code in your markup</h1>
-                    <p className="as-c fs-2xl lh-3 max-w-100 md:max-w-full ta-c">Packaged with components and utility classes to get your applications into production.</p>
+                    <h1 className="as-c d-f fs-6xl fw-700 lg:text-6xl lh-1">Quickly build applications with less code in your markup</h1>
+                    <p className="as-c fs-xl lh-3 max-w-100 md:max-w-full ta-c">Packaged with all of the necessary components and utility classes to facilitate the transition of your applications from development to production.</p>
                     <section className="as-c d-f fd-c mt-6 sm:fd-r sm:w-auto">
-                        <Link className="ai-c bg-pink h:t-white d-if fs-md fw-700 jc-c lh-3 px-3 py-3 rad-8 sm:px-6 sm:w-auto t-white w-full" to="/docs/installation">Get started</Link>
+                        <Link className="ai-c bg-pink h:t-white d-if fs-sm fw-700 jc-c lh-3 px-3 py-3 rad-2 sm:px-6 sm:w-auto t-white w-full" to="/docs/installation">Get started</Link>
                     </section>
                 </main>
             </div>
