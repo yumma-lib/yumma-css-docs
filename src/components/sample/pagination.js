7b6ef225b414fe77e6a3ff8e725315cb18134330
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-07-08 21:31:15 +0100

    docs: update useVersion in the component pages

--- src/components/sample/pagination.js
+++ src/components/sample/pagination.js
@@ -1,32 +1,32 @@
 export const basic = `
 <nav class="ai-c cg-1 d-f">
-  <button class="ai-c bg-transparent cg-1 d-if fs-sm h:bg-silver-2 jc-c min-h-10 min-w-10 px-1 py-2 rad-2 t-d-silver-5">
-    <p class="fw-600">Previous</p>
+  <button class="ai-c bg-transparent cg-1 d-if fs-sm fw-500 h:bg-l-silver-1 h:t-white jc-c min-h-10 min-w-10 px-1 py-2 rad-2 t-d-silver-5">
+    Previous
   </button>
   <div class="ai-c cg-1 d-f">
-    <button class="ai-c bg-transparent d-f fs-sm h:bg-silver-2 jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-d-silver-5">1</button>
-    <button class="ai-c bg-transparent d-f fs-sm h:bg-silver-2 jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-d-silver-5">2</button>
-    <button class="ai-c bg-transparent d-f fs-sm h:bg-silver-2 jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-d-silver-5">3</button>
+    <button class="ai-c bg-transparent d-f fs-sm h:bg-l-silver-1 h:t-white jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-d-silver-5">1</button>
+    <button class="ai-c bg-transparent d-f fs-sm h:bg-l-silver-1 h:t-white jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-d-silver-5">2</button>
+    <button class="ai-c bg-transparent d-f fs-sm h:bg-l-silver-1 h:t-white jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-d-silver-5">3</button>
   </div>
-  <button class="ai-c bg-transparent cg-1 d-if fs-sm h:bg-silver-2 jc-c min-h-10 min-w-10 px-1 py-2 rad-2 t-d-silver-5">
-    <p class="fw-600">Next</p>
+  <button class="ai-c bg-transparent cg-1 d-if fs-sm h:bg-l-silver-1 h:t-white jc-c min-h-10 min-w-10 px-1 py-2 rad-2 t-d-silver-5">
+    <p class="fw-500">Next</p>
   </button>
 </nav>
 `;
 
 export const withBorder = `
 <nav class="ai-c cg-1 d-f">
-  <button class="ai-c bg-transparent cg-2 d-if fs-sm h:bg-l-silver-2 jc-c min-h-10 min-w-10 px-1 py-2 rad-2 t-silver">
+  <button class="ai-c bg-transparent cg-2 d-if fs-sm h:bg-l-silver-1 h:t-white jc-c min-h-10 min-w-10 px-1 py-2 rad-2 t-d-silver-4">
     <svg class="dim-4 fs-0" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M15 6L9 12L15 18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
     </svg>
   </button>
   <div class="ai-c cg-1 d-f">
-    <button class="ai-c b-1 bc-silver-2 bg-transparent d-f fs-sm jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-silver">1</button>
-    <button class="ai-c bg-transparent d-f fs-sm h:bg-l-silver-2 jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-silver">2</button>
-    <button class="ai-c bg-transparent d-f fs-sm h:bg-l-silver-2 jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-silver">3</button>
+    <button class="ai-c b-1 bc-l-silver-1 bg-transparent d-f fs-sm jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-d-silver-4">1</button>
+    <button class="ai-c bg-transparent d-f fs-sm h:bg-l-silver-1 h:t-white jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-d-silver-4">2</button>
+    <button class="ai-c bg-transparent d-f fs-sm h:bg-l-silver-1 h:t-white jc-c min-h-10 min-w-10 px-3 py-2 rad-2 t-d-silver-4">3</button>
   </div>
-  <button class="ai-c bg-transparent cg-2 d-if fs-sm h:bg-l-silver-2 jc-c min-h-10 min-w-10 px-1 py-2 rad-2 t-silver">
+  <button class="ai-c bg-transparent cg-2 d-if fs-sm h:bg-l-silver-1 h:t-white jc-c min-h-10 min-w-10 px-1 py-2 rad-2 t-d-silver-4">
     <svg class="dim-4 fs-0" width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 6L15 12L9 18" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
   </button>
 </nav>
