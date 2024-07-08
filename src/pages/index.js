b391ed89fa665f591e3f75e27d93bda0c7e406a3
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-07-09 00:34:56 +0100

    docs: update index page styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,12 @@ export default function Index() {
 
     return (
         <Layout description={description}>
-            <div className="mx-2">
-                <main style={{ maxWidth: '56rem' }} className="d-f fd-c jc-c lg:mb-30 lg:mt-24 mb-18 mt-12 mx-auto ta-c">
-                    <h1 className="as-c d-f fs-6xl fw-700 lg:text-6xl lh-1">Quickly build applications with less code in your markup</h1>
-                    <p className="as-c fs-xl lh-3 max-w-100 md:max-w-full ta-c">Packaged with all of the necessary components and utility classes to facilitate the transition of your applications from development to production.</p>
-                    <section className="as-c d-f fd-c mt-6 sm:fd-r sm:w-auto">
-                        <Link className="ai-c bg-pink h:t-white d-if fs-sm fw-700 jc-c lh-3 px-3 py-3 rad-2 sm:px-6 sm:w-auto t-white w-full" to="/docs/installation">Get started</Link>
+            <div class="mx-2">
+                <main style={{ maxWidth: '56rem' }} class="d-f fd-c jc-c lg:mb-30 lg:mt-24 mb-18 mt-12 mx-auto ta-c">
+                    <h1 class="as-c d-f fs-6xl fw-700 lg:text-6xl lh-1">Quickly build applications with less code in your markup</h1>
+                    <p class="as-c fs-xl lh-3 max-w-100 md:max-w-full ta-c">Packaged with all of the necessary components and utility classes to facilitate the transition of your applications from development to production.</p>
+                    <section class="as-c d-f fd-c mt-6 sm:fd-r sm:w-auto">
+                        <Link style={{ textDecoration: 'none' }} class="ai-c bg-pink d-if fs-sm fw-700 h:bg-d-pink-1 h:t-white jc-c lh-3 px-3 py-3 rad-2 sm:px-6 sm:w-auto t-white w-full" to="/docs/installation">Get started</Link>
                     </section>
                 </main>
             </div>
