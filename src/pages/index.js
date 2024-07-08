ff761c609daa879d2a3ddf302b055ebf5ff3b49a
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-07-09 00:38:49 +0100

    docs: update index page styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Index() {
                     <h1 class="as-c d-f fs-6xl fw-700 lg:text-6xl lh-1">Quickly build applications with less code in your markup</h1>
                     <p class="as-c fs-xl lh-3 max-w-100 md:max-w-full ta-c">Packaged with all of the necessary components and utility classes to facilitate the transition of your applications from development to production.</p>
                     <section class="as-c d-f fd-c mt-6 sm:fd-r sm:w-auto">
-                        <Link style={{ textDecoration: 'none' }} class="ai-c bg-pink d-if fs-sm fw-700 h:bg-d-pink-1 h:t-white jc-c lh-3 px-3 py-3 rad-2 sm:px-6 sm:w-auto t-white w-full" to="/docs/installation">Get started</Link>
+                        <Link style={{ textDecoration: 'none' }} class="ai-c bg-pink d-if fs-sm fw-700 h:t-white jc-c lh-3 px-3 py-3 rad-2 sm:px-6 sm:w-auto t-white w-full" to="/docs/installation">Get started</Link>
                     </section>
                 </main>
             </div>
