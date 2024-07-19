8d9b27697cc8a4ced24ce3940987eb277c4710cb
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-07-19 20:18:01 +0100

    docs: add input components

---
+++ src/components/sample/input.js
@@ -0,0 +1,11 @@
+export const rounded = `
+<div class="max-w-100">
+  <input class="b-1 bc-l-silver-2 bg-white d-b fs-sm px-4 py-3 rad-1 w-full" placeholder="Email">
+</div>
+`;
+
+export const pilled = `
+<div class="max-w-100">
+  <input class="b-1 bc-l-silver-2 bg-white d-b fs-sm px-4 py-3 rad-8 w-full" placeholder="Email">
+</div>
+`;
