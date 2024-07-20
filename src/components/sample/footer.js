08c6a97349af61bd97f764d4cb17a922f845be04
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-07-20 01:38:50 +0100

    docs: update footer component

--- src/components/sample/footer.js
+++ src/components/sample/footer.js
@@ -8,17 +8,19 @@ export const base = `
       </div>
 
       <div>
-        <h4 class="fs-xs fw-600 t-d-gray-6">GETTING STARTED</h4>
+        <h4 class="fs-xs fw-600 t-d-gray-6">PRODUCTS</h4>
         <div class="fs-sm mt-3 s-y-3">
-          <p><a class="h:t-d-gray-4 t-gray" href="#">Installation</a></p>
+          <p><a class="h:t-d-gray-4 t-gray" href="#">API</a></p>
+          <p><a class="h:t-d-gray-4 t-gray" href="#">Integrations</a></p>
         </div>
       </div>
 
       <div>
-        <h4 class="fs-xs fw-600 t-d-gray-6">SERVICES</h4>
+        <h4 class="fs-xs fw-600 t-d-gray-6">SOLUTIONS</h4>
         <div class="fs-sm mt-3 s-y-3">
-          <p><a class="h:t-d-gray-4 t-gray" href="#">Templates</a></p>
+          <p><a class="h:t-d-gray-4 t-gray" href="#">Marketing</a></p>
           <p><a class="h:t-d-gray-4 t-gray" href="#">Support</a></p>
+          <p><a class="h:t-d-gray-4 t-gray" href="#">Documentation</a></p>
         </div>
       </div>
 
@@ -26,17 +28,17 @@ export const base = `
         <h4 class="fs-xs fw-600 t-d-gray-6">RESOURCES</h4>
         <div class="fs-sm mt-3 s-y-3">
           <p><a class="h:t-d-gray-4 t-gray" href="#">Blog</a></p>
-          <p><a class="h:t-d-gray-4 t-gray" href="#">Figma</a></p>
+          <p><a class="h:t-d-gray-4 t-gray" href="#">Help center</a></p>
           <p><a class="h:t-d-gray-4 t-gray" href="#">Screencasts</a></p>
         </div>
       </div>
 
       <div>
-        <h4 class="fs-xs fw-600 t-d-gray-6">COMPANY</h4>
+        <h4 class="fs-xs fw-600 t-d-gray-6">DEVELOPERS</h4>
         <div class="fs-sm mt-3 s-y-3">
-          <p><a class="h:t-d-gray-4 t-gray" href="#">About us</a></p>
-          <p><a class="t-gray h:t-d-gray-4" href="#">Jobs</a> <span class="t-blue">— We're hiring</span></p>
-          <p><a class="h:t-d-gray-4 t-gray" href="#">Contact us</a></p>
+          <p><a class="h:t-d-gray-4 t-gray" href="#">Get started</a></p>
+          <p><a class="t-gray h:t-d-gray-4" href="#">API docs</a> <span class="t-blue">— We're hiring</span></p>
+          <p><a class="h:t-d-gray-4 t-gray" href="#">Comumnity</a></p>
         </div>
       </div>
     </div>
