d80fb93faf7fdadd32e164dcd65ae4cd6b4ebe7d
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-07-20 01:16:48 +0100

    docs: add table components

---
+++ src/components/sample/table.js
@@ -0,0 +1,80 @@
+export const base = `
+<div class="ovf-x-auto">
+  <table class="b-1 bc-c bc-l-silver-5 min-w-full">
+    <thead class="bg-white">
+      <tr>
+        <th scope="col" class="bb-1 bc-l-silver-5 fs-xs fw-500 px-6 py-3 t-gray ta-l">
+          NAME
+        </th>
+        <th scope="col" class="bb-1 bc-l-silver-5 fs-xs fw-500 px-6 py-3 t-gray ta-l">
+          TITLE
+        </th>
+        <th scope="col" class="bb-1 bc-l-silver-5 fs-xs fw-500 px-6 py-3 t-gray ta-l">
+          STATUS
+        </th>
+        <th scope="col" class="bb-1 bc-l-silver-5 fs-xs fw-500 px-6 py-3 t-gray ta-l">
+          ACTION
+        </th>
+      </tr>
+    </thead>
+    <tbody class="bg-white">
+      <tr>
+        <td class="bb-1 bc-l-silver-5 fs-sm fw-500 px-6 py-4 t-d-gray-6">
+          Jane Cooper
+        </td>
+        <td class="bb-1 bc-l-silver-5 fs-sm px-6 py-4 t-gray">
+          Senior Designer
+        </td>
+        <td class="bb-1 bc-l-silver-5 fs-sm px-6 py-4 t-gray">
+          Active
+        </td>
+        <td class="bb-1 bc-l-silver-5 fs-sm fw-600 h:t-d-blue-4 px-6 py-4 t-blue">
+          <button class="c-p">Remove</button>
+        </td>
+      </tr>
+      <tr>
+        <td class="bb-1 bc-l-silver-5 fs-sm fw-500 px-6 py-4 t-d-gray-6">
+          Cody Fisher
+        </td>
+        <td class="bb-1 bc-l-silver-5 fs-sm px-6 py-4 t-gray">
+          Front-end Developer
+        </td>
+        <td class="bb-1 bc-l-silver-5 fs-sm px-6 py-4 t-gray">
+          Active
+        </td>
+        <td class="bb-1 bc-l-silver-5 fs-sm fw-600 h:t-d-blue-4 px-6 py-4 t-blue">
+          <button class="c-p">Remove</button>
+        </td>
+      </tr>
+      <tr>
+        <td class="bb-1 bc-l-silver-5 fs-sm fw-500 px-6 py-4 t-d-gray-6">
+          Gilbert Jobs
+        </td>
+        <td class="bb-1 bc-l-silver-5 fs-sm px-6 py-4 t-gray">
+          Directives Manager
+        </td>
+        <td class="bb-1 bc-l-silver-5 fs-sm px-6 py-4 t-gray">
+          Active
+        </td>
+        <td class="bb-1 bc-l-silver-5 fs-sm fw-600 h:t-d-blue-4 px-6 py-4 t-blue">
+          <button class="c-p">Remove</button>
+        </td>
+      </tr>
+      <tr>
+        <td class="fs-sm fw-500 px-6 py-4 t-d-gray-6">
+          Jenny Wilson
+        </td>
+        <td class="fs-sm px-6 py-4 t-gray">
+          Tester
+        </td>
+        <td class="fs-sm px-6 py-4 t-gray">
+          Active
+        </td>
+        <td class="fs-sm fw-600 h:t-d-blue-4 px-6 py-4 t-blue">
+          <button class="c-p">Remove</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+`;
