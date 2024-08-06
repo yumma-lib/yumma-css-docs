de60801d8c5d34b975fc6e41c27141fe26e48aea
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-08-06 21:01:21 +0100

    fix: image not showing in the blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
 						Renildo: {
 							name: 'Renildo Pereira',
 							title: 'Maintainer',
-							picture: 'src/assets/renildo.jpg',
+							picture: '/renildo.jpg',
 							url: 'https://x.com/rrenildoo'
 						},
 					},
