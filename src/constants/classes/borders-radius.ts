fdfd2e5a2609588b25b25f9903dfa994cdd647d8
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-24 18:56:42 +0100

    docs: add code visual examples

---
+++ src/constants/classes/borders-radius.ts
@@ -0,0 +1,24 @@
+export const borderRadius: string = `
+<div class="d-g g-16 gtc-4">
+  <button class="bg-cyan dim-16 rad-1 tc-white"></button>
+  <button class="bg-cyan dim-16 rad-2 tc-white"></button>
+  <button class="bg-cyan dim-16 rad-3 tc-white"></button>
+  <button class="bg-cyan dim-16 rad-half tc-white"></button>
+</div>
+`;
+
+export const bottomLeftRadius: string = `
+<button class="bg-cyan dim-16 rad-bl-2 tc-white"></button>
+`;
+
+export const bottomRightRadius: string = `
+<button class="bg-cyan dim-16 rad-br-2 tc-white"></button>
+`;
+
+export const topLeftRadius: string = `
+<button class="bg-cyan dim-16 rad-tl-2 tc-white"></button>
+`;
+
+export const topRightRadius: string = `
+<button class="bg-cyan dim-16 rad-tr-2 tc-white"></button>
+`;
