91cd079bfc0ceb58a78bbe7b33ff6b41e12fc8d8
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-24 01:17:16 +0100

    docs [style]:
    - update headings
    - add visual example for `/border-collapse` page

--- src/constants/classes/background-color.ts
+++ src/constants/classes/background-color.ts
@@ -1,3 +1,3 @@
-export const backgroundColor: string = `
-<button class="c-p bg-cyan bs-sm fw-600 px-5 py-1 rad-2 tc-white">Subscribe</button>
+export const base: string = `
+<button class="bg-cyan dim-16 rad-1 tc-white"></button>
 `;
---
+++ src/constants/classes/border-collapse.ts
@@ -0,0 +1,33 @@
+export const collapse: string = `
+<table class="bg-white b-1 bc-black bc-c w-full">
+  <tr>
+    <th class="b-1 bc-black p-2">Name</th>
+    <th class="b-1 bc-black p-2">Email</th>
+  </tr>
+  <tr>
+    <td class="b-1 bc-black p-2">John Doe</td>
+    <td class="b-1 bc-black p-2">john@example.com</td>
+  </tr>
+  <tr>
+    <td class="b-1 bc-black p-2">Jane Smith</td>
+    <td class="b-1 bc-black p-2">jane@example.com</td>
+  </tr>
+</table>
+`;
+
+export const separate: string = `
+<table class="bg-white b-1 bc-black bc-s w-full">
+  <tr>
+    <th class="b-1 bc-black p-2">Name</th>
+    <th class="b-1 bc-black p-2">Email</th>
+  </tr>
+  <tr>
+    <td class="b-1 bc-black p-2">John Doe</td>
+    <td class="b-1 bc-black p-2">john@example.com</td>
+  </tr>
+  <tr>
+    <td class="b-1 bc-black p-2">Jane Smith</td>
+    <td class="b-1 bc-black p-2">jane@example.com</td>
+  </tr>
+</table>
+`;
