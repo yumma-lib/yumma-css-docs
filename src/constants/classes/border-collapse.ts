34ebcf0dda2475b9275b9034038bb7453ecab482
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-24 01:27:59 +0100

    change border color

--- src/constants/classes/border-collapse.ts
+++ src/constants/classes/border-collapse.ts
@@ -1,33 +1,33 @@
 export const collapse: string = `
-<table class="bg-white b-1 bc-black bc-c w-full">
+<table class="bg-white b-1 bc-silver bc-c w-full">
   <tr>
-    <th class="b-1 bc-black p-2">Name</th>
-    <th class="b-1 bc-black p-2">Email</th>
+    <th class="b-1 bc-silver p-2">Name</th>
+    <th class="b-1 bc-silver p-2">Email</th>
   </tr>
   <tr>
-    <td class="b-1 bc-black p-2">John Doe</td>
-    <td class="b-1 bc-black p-2">john@example.com</td>
+    <td class="b-1 bc-silver p-2">John Doe</td>
+    <td class="b-1 bc-silver p-2">john@example.com</td>
   </tr>
   <tr>
-    <td class="b-1 bc-black p-2">Jane Smith</td>
-    <td class="b-1 bc-black p-2">jane@example.com</td>
+    <td class="b-1 bc-silver p-2">Jane Smith</td>
+    <td class="b-1 bc-silver p-2">jane@example.com</td>
   </tr>
 </table>
 `;
 
 export const separate: string = `
-<table class="bg-white b-1 bc-black bc-s w-full">
+<table class="bg-white b-1 bc-silver bc-s w-full">
   <tr>
-    <th class="b-1 bc-black p-2">Name</th>
-    <th class="b-1 bc-black p-2">Email</th>
+    <th class="b-1 bc-silver p-2">Name</th>
+    <th class="b-1 bc-silver p-2">Email</th>
   </tr>
   <tr>
-    <td class="b-1 bc-black p-2">John Doe</td>
-    <td class="b-1 bc-black p-2">john@example.com</td>
+    <td class="b-1 bc-silver p-2">John Doe</td>
+    <td class="b-1 bc-silver p-2">john@example.com</td>
   </tr>
   <tr>
-    <td class="b-1 bc-black p-2">Jane Smith</td>
-    <td class="b-1 bc-black p-2">jane@example.com</td>
+    <td class="b-1 bc-silver p-2">Jane Smith</td>
+    <td class="b-1 bc-silver p-2">jane@example.com</td>
   </tr>
 </table>
 `;
