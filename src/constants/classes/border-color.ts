4df0a0b25a7326a669876b98460bb4e21d9031cb
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-24 14:51:52 +0100

    docs: add code visual examples

---
+++ src/constants/classes/border-color.ts
@@ -0,0 +1,34 @@
+export const borderColor: string = `
+<div class="b-2 bc-cyan bg-white p-4 w-64">
+  <h2 class="fw-600 mb-2">Card Title</h2>
+  <p class="tc-gray tc-gray-700">This is a simple card with a cyan border.</p>
+</div>
+`;
+
+export const borderBottomColor: string = `
+<div class="b-2 bc-b-cyan bg-white p-4 w-64">
+  <h2 class="fw-600 mb-2">Card Title</h2>
+  <p class="tc-gray tc-gray-700">This is a simple card with a cyan border.</p>
+</div>
+`;
+
+export const borderLeftColor: string = `
+<div class="b-2 bc-l-cyan bg-white p-4 w-64">
+  <h2 class="fw-600 mb-2">Card Title</h2>
+  <p class="tc-gray tc-gray-700">This is a simple card with a cyan border.</p>
+</div>
+`;
+
+export const borderRightColor: string = `
+<div class="b-2 bc-r-cyan bg-white p-4 w-64">
+  <h2 class="fw-600 mb-2">Card Title</h2>
+  <p class="tc-gray tc-gray-700">This is a simple card with a cyan border.</p>
+</div>
+`;
+
+export const borderTopColor: string = `
+<div class="b-2 bc-t-cyan bg-white p-4 w-64">
+  <h2 class="fw-600 mb-2">Card Title</h2>
+  <p class="tc-gray tc-gray-700">This is a simple card with a cyan border.</p>
+</div>
+`;
