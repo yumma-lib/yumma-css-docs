3d0a13444bb985742b386684d621d3d5c14a5d09
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-25 16:44:01 +0100

    docs: add code visual examples

---
+++ src/constants/examples/border-width.ts
@@ -0,0 +1,24 @@
+export const base: string = `
+<div class="d-g g-16 gtc-4">
+  <button class="bg-white dim-16 b-1 bs-solid bc-cyan tc-cyan">1px</button>
+  <button class="bg-white dim-16 b-2 bs-solid bc-cyan tc-cyan">2px</button>
+  <button class="bg-white dim-16 b-3 bs-solid bc-cyan tc-cyan">4px</button>
+  <button class="bg-white dim-16 b-4 bs-solid bc-cyan tc-cyan">6px</button>
+</div>
+`;
+
+export const bottomWidth: string = `
+<button class="bg-white dim-16 bb-3 bs-solid bc-cyan tc-cyan">4px</button>
+`;
+
+export const leftWidth: string = `
+<button class="bg-white dim-16 bl-3 bs-solid bc-cyan tc-cyan">4px</button>
+`;
+
+export const rightWidth: string = `
+<button class="bg-white dim-16 br-3 bs-solid bc-cyan tc-cyan">4px</button>
+`;
+
+export const topWidth: string = `
+<button class="bg-white dim-16 bt-3 bs-solid bc-cyan tc-cyan">4px</button>
+`;
