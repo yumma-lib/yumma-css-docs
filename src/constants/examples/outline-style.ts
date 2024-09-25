ce3ec11676b310fe14cdcf075c1d81740aea0835
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-25 21:19:28 +0100

    docs: add code visual examples

---
+++ src/constants/examples/outline-style.ts
@@ -0,0 +1,8 @@
+export const base: string = `
+<div class="d-g g-16 gtc-4">
+    <button class="b-1 bc-l-silver-6 bg-white fw-600 oc-cyan oo-1 os-none px-5 py-1 rad-1">Subscribe</button>
+    <button class="b-1 bc-l-silver-6 bg-white fw-600 oc-cyan oo-1 os-d px-5 py-1 rad-1">Subscribe</button>
+    <button class="b-1 bc-l-silver-6 bg-white fw-600 oc-cyan oo-1 os-h px-5 py-1 rad-1">Subscribe</button>
+    <button class="b-1 bc-l-silver-6 bg-white fw-600 oc-cyan oo-1 os-s px-5 py-1 rad-1">Subscribe</button>
+</div>
+`;
