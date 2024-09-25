62adfa4d950a651deea0dab3dfac88a99ec49853
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-25 22:25:52 +0100

    docs: add code visual examples

---
+++ src/constants/examples/box-sizing.ts
@@ -0,0 +1,15 @@
+export const borderBox: string = `
+<div class="b-1 bc-cyan bg-white dim-32">
+  <div class="b-1 bc-cyan bs-bb dim-32 p-5">
+    <div class="b-1 bc-cyan bg-cyan dim-full"></div>
+  </div>
+</div>
+`;
+
+export const contentBox: string = `
+<div class="b-1 bc-cyan bg-white dim-32">
+  <div class="b-1 bc-cyan bs-cb dim-32 p-5">
+    <div class="b-1 bc-cyan bg-cyan dim-full"></div>
+  </div>
+</div>
+`;
