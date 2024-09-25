38a326dfc2d2a095a4b9955cc26e2eb8089395ab
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-25 21:31:16 +0100

    docs [fix]: - remove extra div - restore `base` const in `border-color.ts` file

--- src/constants/examples/border-style.ts
+++ src/constants/examples/border-style.ts
@@ -1,5 +1,5 @@
 export const borderStyle: string = `
-<div class="d-g g-16 gtc-4"></div>
+<div class="d-g g-16 gtc-4">
   <button class="b-2 b-none bc-cyan bg-white dim-16 tc-cyan">None</button>
   <button class="b-2 b-d bc-cyan bg-white dim-16 tc-cyan">Dashed</button>
   <button class="b-2 b-h bc-cyan bg-white dim-16 tc-cyan">Hidden</button>
