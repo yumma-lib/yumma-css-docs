2f0ea277173d3461f1075155f08dc4ad9ad0a788
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-27 23:34:11 +0100

    docs: add code visual examples

---
+++ src/constants/examples/height.ts
@@ -0,0 +1,8 @@
+export const base: string = `
+<div class="d-g g-16 gtc-4">
+  <div class="bg-cyan h-10 fw-700 ins rad-1 tc-white p-4"></div>
+  <div class="bg-cyan h-20 fw-700 ins rad-1 tc-white p-4"></div>
+  <div class="bg-cyan h-30 fw-700 ins rad-1 tc-white p-4"></div>
+  <div class="bg-cyan h-40 fw-700 ins rad-1 tc-white p-4"></div>
+</div>
+`;
