7ab0cfe2859f2e1dccd0c433e854f7dbea3e8080
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-28 00:16:44 +0100

    docs: add code visual examples

--- src/constants/examples/height.ts
+++ src/constants/examples/height.ts
@@ -1,8 +1,15 @@
 export const base: string = `
 <div class="d-g g-16 gtc-4">
-  <div class="bg-cyan h-10 fw-700 ins rad-1 tc-white p-4"></div>
-  <div class="bg-cyan h-20 fw-700 ins rad-1 tc-white p-4"></div>
-  <div class="bg-cyan h-30 fw-700 ins rad-1 tc-white p-4"></div>
-  <div class="bg-cyan h-40 fw-700 ins rad-1 tc-white p-4"></div>
+  <div class="bg-cyan fw-700 h-8 ins ins p-2 rad-1 tc-white">2rem</div>
+  <div class="bg-cyan fw-700 h-16 ins ins p-2 rad-1 tc-white">4rem</div>
+  <div class="bg-cyan fw-700 h-24 ins ins p-2 rad-1 tc-white">6rem</div>
+  <div class="bg-cyan fw-700 h-32 ins ins p-2 rad-1 tc-white">8rem</div>
+</div>
+`;
+
+export const heightPercentages: string = `
+<div class="b-3 b-d bc-cyan d-g g-16 gtc-2 h-64 p-2 rad-2">
+  <div class="bg-cyan fw-700 h-full ins p-2 rad-1 tc-white">100%</div>
+  <div class="bg-cyan fw-700 h-half ins p-2 rad-1 tc-white">50%</div>
 </div>
 `;
