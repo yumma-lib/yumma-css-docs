2183fe9efe5fbd42d1436141608c85500a22f55e
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-27 22:47:49 +0100

    docs: add code visual examples

---
+++ src/constants/examples/dimension.ts
@@ -0,0 +1,8 @@
+export const base: string = `
+<div class="d-if fd-r s-x-16">
+  <div class="bg-cyan dim-16 fw-700 ins rad-2 tc-white">4rem</div>
+  <div class="bg-cyan dim-20 fw-700 ins rad-2 tc-white">5rem</div>
+  <div class="bg-cyan dim-24 fw-700 ins rad-2 tc-white">6rem</div>
+  <div class="bg-cyan dim-28 fw-700 ins rad-2 tc-white">7rem</div>
+</div>
+`;
