7103c5ec9bd0c78686266cfb1f8f447f55d3d3d0
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-28 23:06:56 +0100

    docs: add code visual examples

---
+++ src/constants/examples/padding.ts
@@ -0,0 +1,69 @@
+export const base: string = `
+<div class="bg-cyan rad-2 fw-700 tc-white">
+  <div class="area">
+    <div class="p-8">
+        <p class="bg-cyan tc-white p-4">2rem</p>
+    </div>
+  </div>
+</div>
+`;
+
+export const paddingBottom: string = `
+<div class="bg-cyan rad-2 fw-700 tc-white">
+  <div class="area">
+    <div class="pb-8">
+        <p class="bg-cyan tc-white p-4">2rem</p>
+    </div>
+  </div>
+</div>
+`;
+
+export const paddingLeft: string = `
+<div class="bg-cyan rad-2 fw-700 tc-white">
+  <div class="area">
+    <div class="pl-8">
+        <p class="bg-cyan tc-white p-4">2rem</p>
+    </div>
+  </div>
+</div>
+`;
+
+export const paddingRight: string = `
+<div class="bg-cyan rad-2 fw-700 tc-white">
+  <div class="area">
+    <div class="pr-8">
+        <p class="bg-cyan tc-white p-4">2rem</p>
+    </div>
+  </div>
+</div>
+`;
+
+export const paddingTop: string = `
+<div class="bg-cyan rad-2 fw-700 tc-white">
+  <div class="area">
+    <div class="pt-8">
+        <p class="bg-cyan tc-white p-4">2rem</p>
+    </div>
+  </div>
+</div>
+`;
+
+export const paddingX: string = `
+<div class="bg-cyan rad-2 fw-700 tc-white">
+  <div class="area">
+    <div class="px-8">
+        <p class="bg-cyan tc-white p-4">2rem</p>
+    </div>
+  </div>
+</div>
+`;
+
+export const paddingY: string = `
+<div class="bg-cyan rad-2 fw-700 tc-white">
+  <div class="area">
+    <div class="py-8">
+        <p class="bg-cyan tc-white p-4">2rem</p>
+    </div>
+  </div>
+</div>
+`;
