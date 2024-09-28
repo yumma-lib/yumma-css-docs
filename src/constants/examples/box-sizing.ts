71606082f434345d662f366008d476d44020e437
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-28 18:18:03 +0100

    add `area` class

--- src/constants/examples/box-sizing.ts
+++ src/constants/examples/box-sizing.ts
@@ -1,11 +1,11 @@
-export const borderBox: string = `
-<div class="dim-26 b-2 bc-cyan bs-bb p-8">
-  <div class="bg-cyan dim-full fw-700 ins tc-white">box</div>
+export const contentBox: string = `
+<div class="area dim-26 b-2 bc-cyan bs-cb p-8">
+  <div class="bg-cyan dim-full fw-700 ins tc-white">Content</div>
 </div>
 `;
 
-export const contentBox: string = `
-<div class="dim-26 b-2 bc-cyan bs-cb p-8">
-  <div class="bg-cyan dim-full fw-700 ins tc-white">content</div>
+export const borderBox: string = `
+<div class="area dim-26 b-2 bc-cyan bs-bb p-8 ">
+  <div class="bg-cyan dim-full fw-700 ins tc-white">Box</div>
 </div>
 `;
