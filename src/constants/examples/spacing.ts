78db6ae332c704a4457b83b9dcd46be60a9a6454
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-28 23:14:57 +0100

    docs: add code visual examples

---
+++ src/constants/examples/spacing.ts
@@ -0,0 +1,15 @@
+export const spacingX: string = `
+<div class="area d-if fd-r s-x-8">
+  <div class="bg-cyan dim-16 fw-700 ins rad-2 tc-white">A</div>
+  <div class="bg-cyan dim-16 fw-700 ins rad-2 tc-white">B</div>
+  <div class="bg-cyan dim-16 fw-700 ins rad-2 tc-white">C</div>
+</div>
+`;
+
+export const spacingY: string = `
+<div class="area d-if fd-c s-y-8">
+  <div class="bg-cyan dim-16 fw-700 ins rad-2 tc-white">A</div>
+  <div class="bg-cyan dim-16 fw-700 ins rad-2 tc-white">B</div>
+  <div class="bg-cyan dim-16 fw-700 ins rad-2 tc-white">C</div>
+</div>
+`;
