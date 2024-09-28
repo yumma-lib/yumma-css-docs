a4c7734442d2169f89a5bd71b5105c894cdf1c2a
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-28 18:03:25 +0100

    docs: add code visual examples

---
+++ src/constants/examples/margin.ts
@@ -0,0 +1,41 @@
+export const base: string = `
+<div class="area rad-2">
+    <div class="bg-cyan dim-16 fw-700 ins m-8 rad-2 tc-white">2rem</div>
+</div>
+`;
+
+export const marginBottom: string = `
+<div class="area rad-2">
+    <div class="bg-cyan dim-16 fw-700 ins mb-8 rad-2 tc-white">2rem</div>
+</div>
+`;
+
+export const marginLeft: string = `
+<div class="area rad-2">
+    <div class="bg-cyan dim-16 fw-700 ins ml-8 rad-2 tc-white">2rem</div>
+</div>
+`;
+
+export const marginRight: string = `
+<div class="area rad-2">
+    <div class="bg-cyan dim-16 fw-700 ins mr-8 rad-2 tc-white">2rem</div>
+</div>
+`;
+
+export const marginTop: string = `
+<div class="area rad-2">
+    <div class="bg-cyan dim-16 fw-700 ins mt-8 rad-2 tc-white">2rem</div>
+</div>
+`;
+
+export const marginX: string = `
+<div class="area rad-2">
+    <div class="bg-cyan dim-16 fw-700 ins mx-8 rad-2 tc-white">2rem</div>
+</div>
+`;
+
+export const marginY: string = `
+<div class="area rad-2">
+    <div class="bg-cyan dim-16 fw-700 ins my-8 rad-2 tc-white">2rem</div>
+</div>
+`;
