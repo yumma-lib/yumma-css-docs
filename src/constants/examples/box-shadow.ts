b5535aa6e0f6b532c770a2f05731d7e7030ef3f6
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-29 18:57:22 +0100

    update output values

--- src/constants/examples/box-shadow.ts
+++ src/constants/examples/box-shadow.ts
@@ -1,8 +1,8 @@
 export const base: string = `
 <div class="d-g g-16 gtc-4">
-    <div class="bg-white bs-xs dim-16 fw-700 ins rad-2 tc-cyan">sm</div>
-    <div class="bg-white bs-sm dim-16 fw-700 ins rad-2 tc-cyan">md</div>
-    <div class="bg-white bs-md dim-16 fw-700 ins rad-2 tc-cyan">lg</div>
+    <div class="bg-white bs-xs dim-16 fw-700 ins rad-2 tc-cyan">xs</div>
+    <div class="bg-white bs-sm dim-16 fw-700 ins rad-2 tc-cyan">sm</div>
+    <div class="bg-white bs-md dim-16 fw-700 ins rad-2 tc-cyan">md</div>
     <div class="bg-white bs-lg dim-16 fw-700 ins rad-2 tc-cyan">lg</div>
 </div>
 `;
