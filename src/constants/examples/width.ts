1d53aedecefb1c4b5dee11f4d0b122cf086dc2cb
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-29 01:48:42 +0100

    docs: add code visual examples

---
+++ src/constants/examples/width.ts
@@ -0,0 +1,15 @@
+export const base: string = `
+<div class="s-y-8">
+  <div class="bg-cyan fw-700 ins p-2 rad-1 tc-white w-12">2rem</div>
+  <div class="bg-cyan fw-700 ins p-2 rad-1 tc-white w-20">4rem</div>
+  <div class="bg-cyan fw-700 ins p-2 rad-1 tc-white w-28">6rem</div>
+  <div class="bg-cyan fw-700 ins p-2 rad-1 tc-white w-36">8rem</div>
+</div>
+`;
+
+export const widthPercentages: string = `
+<div class="b-3 b-d bc-cyan p-2 rad-2 s-y-8 w-64">
+  <div class="bg-cyan fw-700 ins p-2 rad-1 tc-white w-full">100%</div>
+  <div class="bg-cyan fw-700 ins p-2 rad-1 tc-white w-half">50%</div>
+</div>
+`;
