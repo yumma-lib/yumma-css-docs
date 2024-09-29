6e62358b94836fb232bf39cb463edaaad35e34bc
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-29 19:01:44 +0100

    docs: add code visual examples

---
+++ src/constants/examples/opacity.ts
@@ -0,0 +1,8 @@
+export const base: string = `
+<div class="d-g g-16 gtc-4">
+    <div class="bg-cyan o-20 dim-16 fw-700 ins rad-2 tc-white">20</div>
+    <div class="bg-cyan o-50 dim-16 fw-700 ins rad-2 tc-white">50</div>
+    <div class="bg-cyan o-70 dim-16 fw-700 ins rad-2 tc-white">70</div>
+    <div class="bg-cyan o-100 dim-16 fw-700 ins rad-2 tc-white">100</div>
+</div>
+`;
