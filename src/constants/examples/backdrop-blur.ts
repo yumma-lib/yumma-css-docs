ddd1d85afe5a09b7a293dd0d3b06a8a86453a5f3
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-29 18:47:36 +0100

    docs: add code visual examples

---
+++ src/constants/examples/backdrop-blur.ts
@@ -0,0 +1,16 @@
+export const base: string = `
+<div class="d-g g-16 gtc-3">
+    <div class="dim-32 p-r rad-2">
+        <img class="dim-full of-c rad-2" src="/public/assets/images/mountain.png" alt="Mountain">
+        <div class="bf-b-sm dim-20 dir-i-0 fw-700 ins m-auto p-a tc-white">sm</div>
+    </div>
+    <div class="dim-32 p-r rad-2">
+        <img class="dim-full of-c rad-2" src="/public/assets/images/mountain.png" alt="Mountain">
+        <div class="bf-b-md dim-20 dir-i-0 fw-700 ins m-auto p-a tc-white">md</div>
+    </div>
+    <div class="dim-32 p-r rad-2">
+        <img class="dim-full of-c rad-2" src="/public/assets/images/mountain.png" alt="Mountain">
+        <div class="bf-b-lg dim-20 dir-i-0 fw-700 ins m-auto p-a tc-white">lg</div>
+    </div>
+</div>
+`;
