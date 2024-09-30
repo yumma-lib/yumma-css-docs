7310d01ec0c81dd4248e09b6390dfb1d43ee0677
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-09-30 02:12:31 +0100

    docs: add code visual examples

---
+++ src/constants/examples/align-content.ts
@@ -0,0 +1,89 @@
+export const baseline: string = `
+<div class="ac-b area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const center: string = `
+<div class="ac-c area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const flexEnd: string = `
+<div class="ac-fe area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const flexStart: string = `
+<div class="ac-fs area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const normal: string = `
+<div class="ac-n area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const stretch: string = `
+<div class="ac-stretch area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const spaceAround: string = `
+<div class="ac-sa area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const spaceBetween: string = `
+<div class="ac-sb area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const spaceEvenly: string = `
+<div class="ac-se area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
