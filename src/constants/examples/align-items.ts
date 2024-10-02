e87dd7bcf10e974b68cabaffb7e480c1b531b2f1
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-10-02 01:41:44 +0100

    docs: add code visual examples

---
+++ src/constants/examples/align-items.ts
@@ -0,0 +1,49 @@
+export const baseline: string = `
+<div class="ai-b area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const center: string = `
+<div class="ai-c area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const flexEnd: string = `
+<div class="ai-fe area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const flexStart: string = `
+<div class="ai-fs area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
+
+export const stretch: string = `
+<div class="ai-s area d-g g-4 gtc-3 h-64 tc-white ta-c w-full">
+  <div class="bg-cyan fw-700 ins p-4 rad-2">A</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">B</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">C</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">D</div>
+  <div class="bg-cyan fw-700 ins p-4 rad-2">E</div>
+</div>
+`;
